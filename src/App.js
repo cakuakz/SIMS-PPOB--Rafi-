cac10e042fd04cfbea1d196dc7cc40fe49af2802
cakuakz <rafiirvansyah88@gmail.com>
2024-10-14 00:00:42 +0700

    feat: commit all project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes, } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
 import Registers from './pages/Registers';
@@ -8,24 +8,38 @@ import Topup from './pages/Topup';
 import ServiceTopup from './pages/ServiceTopup';
 import Transaction from './pages/Transaction';
 import Akun from './pages/Akun';
+import { isLoggedIn } from './utils/constants/storage';
+import { useEffect } from 'react';
 
+const Redirect = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isLoggedIn()) {
+      navigate('/homepage');
+    } else {
+      navigate('/login');
+    }
+  }, [navigate]);
+
+  return null;
+};
 
 function App() {
   return (
-      <BrowserRouter>
-        <Toaster 
-          position='top-right'
-        />
-        <Routes>
-          <Route path='/login' element={<Login />}/>
-          <Route path='/register' element={<Registers />} />
-          <Route path='/homepage' element={<Homepage />} />
-          <Route path='/topup' element={<Topup />}/>
-          <Route path='/servicetopup' element={<ServiceTopup />} />
-          <Route path='/transaction' element={<Transaction />} />
-          <Route path='/akun' element={<Akun />} />
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Toaster position='top-right' />
+      <Routes>
+        <Route path='/' element={<Redirect />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Registers />} />
+        <Route path='/homepage' element={<Homepage />} />
+        <Route path='/topup' element={<Topup />} />
+        <Route path='/servicetopup' element={<ServiceTopup />} />
+        <Route path='/transaction' element={<Transaction />} />
+        <Route path='/akun' element={<Akun />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
